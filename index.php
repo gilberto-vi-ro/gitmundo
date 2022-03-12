a31f7a840998f6b0af9dd3af49ba4b0e8bbcf25f
Gilbert <gilberto.vi.ro@gmail.com>
2022-03-12 12:30:43 -0600

    Primer Screenshot del proyecto gitmundo "091710898 Gilberto"

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Gilberto DGS 8vo</title>
+
+</head>
+<body>
+    <font size="14" name="Tempus Sans">
+        <marquee>
+            SALUDOS CORDIALES
+        </marquee>
+    </font>
+</body>
+
+</html>
